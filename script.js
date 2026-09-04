d5f4e52fc42705d24195ad5844da0fc529595c70
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-04 22:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function toCase(text) {
-    return text.toLowerCase() + "-" + text.toUpperCase();
+
+	if(text.length == 0) return "";
+    let s =  text.toLowerCase();
+	let e =  text.toUpperCase();
+	let ans = s + "-" + e;
+
+	return ans;
 }
 // DO not change the code below
 
-const text = prompt("Enter text:");
+// const text = prompt("Enter text:");
 alert(toCase(text));
